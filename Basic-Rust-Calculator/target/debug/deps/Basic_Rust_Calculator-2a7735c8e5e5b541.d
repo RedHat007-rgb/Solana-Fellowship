dab3c7faea9db9c5bda3c273053c4ab422972328
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-19 17:59:02 +0530

    Untrack target/ directory

--- Basic-Rust-Calculator/target/debug/deps/Basic_Rust_Calculator-2a7735c8e5e5b541.d
+++
@@ -1,5 +0,0 @@
-/Users/basanireddy/Documents/2025/Solana-Fellowship/Basic-Rust-Calculator/target/debug/deps/libBasic_Rust_Calculator-2a7735c8e5e5b541.rmeta: src/main.rs
-
-/Users/basanireddy/Documents/2025/Solana-Fellowship/Basic-Rust-Calculator/target/debug/deps/Basic_Rust_Calculator-2a7735c8e5e5b541.d: src/main.rs
-
-src/main.rs:
