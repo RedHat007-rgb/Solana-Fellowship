47b9d7d1d1cc562b8996e6b40e830bedc057cc6c
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-19 19:49:09 +0530

    Implemented functions

--- Basic-Rust-Calculator/src/main.rs
+++ Basic-Rust-Calculator/src/main.rs
@@ -1,4 +1,38 @@
+use std::io;
+
 fn main(){
-    println!("Welecome to Basic Rust Calculator");
+    println!("Welcome to Basic Rust Calculator");
+    println!("Please Enter the Operation add/sub/mul/div)");
+    let mut operation=String::new();
+    io::stdin().read_line(&mut operation).unwrap();
+
+    println!("Please Enter the number 1");
+    let mut input1=String::new();
+    io::stdin().read_line(&mut input1).unwrap();
+    let trimmed =input1.trim();
+    let  num1:f32=trimmed.parse().unwrap();
+
+    println!("Please Enter the number 2");
+    let mut input2=String::new();
+    io::stdin().read_line(&mut input2).unwrap();
+    let trimmed =input2.trim();
+    let  num2:f32=trimmed.parse().unwrap();
+    
+   
 }
 
+fn add(num1:f32,num2:f32)->f32{
+   return num1+num2;
+}
+
+fn substract(num1:f32,num2:f32)->f32{
+    return num1-num2;
+}
+
+fn multiplication(num1:f32,num2:f32)->f32{
+    return num1*num2;
+}
+fn division(num1:f32,num2:f32)->f32{
+   return num1/num2;
+    
+}
