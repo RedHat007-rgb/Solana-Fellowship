e4a3abb08c7a4aa8dd30bb8cc55b1ac0136db5ca
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-20 18:21:21 +0530

    Added rand library

--- Guessing_Game/src/main.rs
+++ Guessing_Game/src/main.rs
@@ -1,9 +1,17 @@
 use std::io::stdin;
 
+use rand::Rng;
+
 fn main() {
     println!("Gueessing game");
     println!("Enter the number to guess:");
     let mut number=String::new();
     stdin().read_line(&mut number).expect("crash here");
-    println!("you entered: {number}")
+    println!("you entered: {number}");
+
+    let random_number=rand::rng().random_range(1..100);
+
+    println!("Secret number is {random_number}")
+
+   
 }
